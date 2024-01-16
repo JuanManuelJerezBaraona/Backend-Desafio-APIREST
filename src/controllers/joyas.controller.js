8b5fbcd5b6d9416008adce65de2d12d3956e925a
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-16 14:04:02 -0300

    Refactor code and update dependencies

--- src/controllers/joyas.controller.js
+++ src/controllers/joyas.controller.js
@@ -1,10 +1,13 @@
 import { joyaModel } from "../models/joya.model.js";
+
 import { getDatabaseError } from "../helpers/database.error.js";
+import pagination from "../helpers/paginator.js";
+import prepareHateoas from "../helpers/hateoas.js";
 
 const read = async (req, res) => {
 
   try {
-    const joyas = await joyaModel.findAll();
+    const joyas = await joyaModel.getAll();
     return res.status(200).json(joyas);
 
   } catch (error) {
--- src/models/joya.model.js
+++ src/models/joya.model.js
@@ -1,13 +1,13 @@
-import "dotenv/config";
-import format from "pg-format";
 import { pool } from "../../database/connection.js";
+import format from "pg-format";
+import createQuery from "../helpers/filter.js";
 
-const findAll = async () => {
+const getAll = async () => {
   const SQLquery = { text: "SELECT * FROM inventario" };
   const response = await pool.query(SQLquery);
   return response.rows;
 };
 
 export const joyaModel = {
-  findAll
+  getAll
 };
